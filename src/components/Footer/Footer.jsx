977d622fe4bdfb4ce945074c5b9d79525472ef05
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-10-28 14:07:51 +0100

    style: create footer, window rotation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
 import '../../styles/Footer.css'
-const Footer = () => {
-    return(
-        <footer>AQUI DEBE IR EL Footer.</footer>
-    )
-}
+import React, { useEffect, useState } from 'react';
 
-export default Footer
+
+const Footer = () => {
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+                setVisible(true);
+            } else {
+                setVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        
+        
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className={`footer ${visible ? 'visible' : ''}`}>
+            <div className="footer-links">
+                <a href="/about">About Us</a>
+                <a href="/contact">Contact Us</a>
+                <a href="/address">Address</a>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -4,10 +4,19 @@ import '../../styles/Hero.css'
 const Hero = () => {
     return(
         <main>
-            <h1>AQUI DEBE IR EL Main</h1>
-            <h2>Esto es un subtitulo</h2>
-
-        </main>
+        <section>
+            <div className='sectionTop'>
+                <img className='backgroundImage' src='https://img.freepik.com/fotos-premium/imaginese-biotecnologos-marinos-utilizando-ingenieria-genetica-generativa_1198283-89240.jpg' alt="Background" />
+                <div className="content">
+                    <h2>Explore with us</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate laboriosam voluptas quaerat deleniti harum quis, explicabo molestiae fuga quia atque ipsam ea officia beatae recusandae et voluptatum quisquam eligendi aut?</p>
+                    <button>
+                        <a className='buttonTop' href='/proyects'>+</a>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </main>
 
 
     )
