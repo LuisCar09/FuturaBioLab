73dde4204d9b3c056bee9e6886d7149ca5e4ca29
Luis Carlos <luiscbravo94@gmail.com>
2024-11-15 17:07:48 +0000

    styles: contactus css improved and responsive is done

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -26,94 +26,110 @@ const ContactUs = () => {
       <section className="section-contactus">
         <div className="contactus-container">
           <h1>Contact us</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum inventore hic. Consectetur reiciendis ipsam nam, asperiores, aut, commodi dolor expedita neque ex distinctio explicabo blanditiis cupiditate praesentium ea. Delectus?</p>
-        </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum inventore hic. Consectetur reiciendis ipsam nam, asperiores, aut, commodi dolor expedita neque.</p>
         
-        <div className="contactus-wetalk">
+          <div className="contactus-wetalk">
           <h2>We talk</h2>
+          </div>
+        
         </div>
         
+        
+
         <div className="contactus-info-container">
-          <div className='contactus-info'>
-               <div className='contactus-addrees'>
-                 <span className="contactus-title">Address</span>
-                 <p>Example street, City</p>
-                 <p>example@info.com</p>
-               </div>
-            <div className='contactus-phone'>
-            <span className="contactus-title">Phone number</span>
+          <div className='contactus-info contactus-info--childrens'>
+
+            <aside>
+            <h2 className="contactus-title">Address</h2>
+            <p>Example street, City</p>
+            <p>example@info.com</p>
+
+
+            <h2 className="contactus-title">Phone number</h2>
             <p>(123) 456 789</p>
+
+
+            <h2 className="contactus-title">Social Media</h2>
+            <div className='icons-container'>
+              <FacebookIcon />
+              <XIcon />
+              <InstagramIcon />
+              <LinkedInIcon />
             </div>
-            <div className='socialmedia-icons'>
-               <span className="contactus-title">Social Media</span>
-               <div className='icons-container'>
-                <FacebookIcon />
-                <XIcon />
-                <InstagramIcon />
-                <LinkedInIcon />
-               </div>
-             </div>
+            </aside>
+
           </div> 
 
-          <form className='contactus-form' onSubmit={handleSubmit}>
-            <div className='contactus-name'>
-              <div className='name-input'>
-                <label>
-                  <span>Name</span>  
-                  <input
-                    type="text"
-                    placeholder="Your first name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                  />
-                </label>
-              </div>
+          <form className='contactus-form contactus-info--childrens' onSubmit={handleSubmit}>
+            <div className='inputs-container'>
+
+            <div className='contactus-form--inputname'>
               
-              <div className='name-input'>
-                <label>
-                 <span>Last Name</span> 
-                  <input
-                    type="text"
-                    placeholder="Your last name"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    required
-                  />
-                </label>
+              <div>
+              <label htmlFor='input-name' >
+                Name
+              </label>
+              <input
+                  id='input-name'
+                  type="text"
+                  
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+              />
               </div>
-            </div>
             
-            <div className='name-input'>
-              <label>
-               <span>Email*</span> 
-                <input 
-                  type="email"
-                  placeholder="Your email" 
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </label>
-            </div>
             
-            <div className='name-input'>
-              <label>
-                <span>Message</span>
-                <textarea className='name-input textarea'
-                  placeholder="Type your message here"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  required
-                />
+            
+              <div>
+              <label htmlFor='input-lastname'>
+               Last Name
               </label>
-            </div>
-            <div className='button-container'>
-             <button className='formbutton' type="submit">Submit</button>
+              <input
+                  id='input-lastname'
+                  type="text"
+                 
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+              />
+              </div>
+            
+          </div>
+          
+          <div className='contactus-form--email'>
+            <label htmlFor='input-email' >
+            Email 
+            </label>
+              <input 
+                id='input-email' 
+                type="email"
+                
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+          </div>
+          
+          <div className='contactus-form--textarea'>
+            <label htmlFor='textarea' >
+              Message
+            </label>
+              <textarea className='textarea'
+              
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+          </div>
+          <div className='button-container'>
+           <button className='formbutton' type="submit">Send</button>
+          </div>
+
             </div>
           </form> 
 
-          <div className="contactus-image">
+          <div className="contactus-image contactus-info--childrens">
             <img src="https://ericnoprotein.wordpress.com/wp-content/uploads/2014/09/agar-agar.jpg?w=640" alt="Example" />
           </div>
         </div>    
