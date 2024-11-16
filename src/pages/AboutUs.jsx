fd6f2f5c1a9818ac4f2d5f96b958c1b907a49b73
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-11-16 13:37:28 +0100

    feat: about us added, css proposal

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,23 @@
+import '../styles/AboutUs.css'
+
 function AboutUs(){
     return(
-        <h1>hola</h1>
+       <main className="main-aboutus">
+         <section className="section-aboutus">
+            <div className="image-container">
+              <img src="https://img.freepik.com/fotos-premium/celulas-algas-verdes-placa-petri-investigacion-microbiologia_1110022-41195.jpg">
+              </img>
+            </div>
+            <div className="aboutus-container">
+              
+                <h1>Futura Biolab</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, assumenda nesciunt delectus modi earum explicabo deserunt perferendis atque tenetur nulla quaerat commodi facere debitis eveniet voluptates eius exercitationem repellat excepturi.</p>
+                <h2>Our mission</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident accusamus, quia praesentium impedit dolorum, hic consequuntur quae odit nulla saepe, error aspernatur alias nemo eius eligendi voluptates quo aut in.</p>
+            </div>
+         </section>
+
+       </main>
     )
 }
 export default AboutUs
