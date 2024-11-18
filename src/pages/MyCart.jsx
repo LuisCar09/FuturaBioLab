8b53cf665b49d22ae37faf4c37e8b87af27fa013
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-11-18 15:26:50 +0100

    feat:mycart.css added, styles:css added,not finished

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,6 +1,16 @@
+import '../styles/MyCart.css'
+
 function MyCart() {
-    return(
-        <h1>My Cart</h1>
+    return (
+        <main className="main-mycart">
+            <section className="section-mycart">
+                <h1 className="title-mycart">My cart</h1>
+                <div className="container-mycart">
+                    <h2>The cart is empty</h2>
+                    <a href="#" className="continue-mycart">Keep buying</a>
+                </div>
+            </section>
+        </main>
     )
 }
 
