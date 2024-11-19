44696e376b7103ae6c542eee35d21f4f42f48b41
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-11-19 17:19:23 +0100

    feat: projects code added, styles: css fro projects added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,113 @@
 import '../styles/Projects.css'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-function Projects(){
+function Projects() {
     return (
-       <main className='main-projects'>
-        <section className='section-projects'>
-            
-        </section>
-       </main>
+        <main className='main-projects'>
+            <section className='section-projects'>
+                <div className='container-projects'>
+                    <div className='image-projects'>
+                        <img src='https://www.envaselia.com/images_blog/que-son-los-bioplasticos.jpg' alt='Descripción del proyecto'></img>
+                    </div>
+                    <div className='info-projects'>
+                        <div className='usercontainer-projects'>
+                            <div className='user-projects'>
+                                <img 
+                                    src='https://i.pinimg.com/474x/f9/ef/f5/f9eff5fd8e045349b31d4641253f628f.jpg' 
+                                    alt='Perfil' 
+                                    className='profile-image'
+                                />
+                                <div className='username-container'>
+                                    <p>Username</p>
+                                    <div className='date-projects'>
+                                        <p>Date</p>
+                                        <p>Time</p>
+                                    </div>
+                                </div>
+                                <MoreVertIcon className='moreverticon-projects' />
+                            </div>
+                        </div>
+                        <div className='nameproject-projects'>
+                            <h1>Name project</h1>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, rem odit? Voluptatum velit in nobis quis suscipit laborum excepturi deserunt! Vitae voluptas earum, voluptate officia illo libero porro deserunt labore.</p>
+                        </div>
+                    <div className='visualizations-projects'>
+                        <p>Visualizations</p>
+                        <FavoriteBorderIcon className='fav-icon-projects' />
+
+                    </div>
+                    </div>
+                </div>
+                <div className='container-projects'>
+                    <div className='image-projects'>
+                        <img src='https://www.envaselia.com/images_blog/que-son-los-bioplasticos.jpg' alt='Descripción del proyecto'></img>
+                    </div>
+                    <div className='info-projects'>
+                        <div className='usercontainer-projects'>
+                            <div className='user-projects'>
+                                <img 
+                                    src='https://i.pinimg.com/474x/f9/ef/f5/f9eff5fd8e045349b31d4641253f628f.jpg' 
+                                    alt='Perfil' 
+                                    className='profile-image'
+                                />
+                                <div className='username-container'>
+                                    <p>Username</p>
+                                    <div className='date-projects'>
+                                        <p>Date</p>
+                                        <p>Time</p>
+                                    </div>
+                                </div>
+                                <MoreVertIcon className='moreverticon-projects' />
+                            </div>
+                        </div>
+                        <div className='nameproject-projects'>
+                            <h1>Name project</h1>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, rem odit? Voluptatum velit in nobis quis suscipit laborum excepturi deserunt! Vitae voluptas earum, voluptate officia illo libero porro deserunt labore.</p>
+                        </div>
+                    <div className='visualizations-projects'>
+                        <p>Visualizations</p>
+                        <FavoriteBorderIcon className='fav-icon-projects' />
+
+                    </div>
+                    </div>
+                </div>
+                <div className='container-projects'>
+                    <div className='image-projects'>
+                        <img src='https://www.envaselia.com/images_blog/que-son-los-bioplasticos.jpg' alt='Descripción del proyecto'></img>
+                    </div>
+                    <div className='info-projects'>
+                        <div className='usercontainer-projects'>
+                            <div className='user-projects'>
+                                <img 
+                                    src='https://i.pinimg.com/474x/f9/ef/f5/f9eff5fd8e045349b31d4641253f628f.jpg' 
+                                    alt='Perfil' 
+                                    className='profile-image'
+                                />
+                                <div className='username-container'>
+                                    <p>Username</p>
+                                    <div className='date-projects'>
+                                        <p>Date</p>
+                                        <p>Time</p>
+                                    </div>
+                                </div>
+                                <MoreVertIcon className='moreverticon-projects' />
+                            </div>
+                        </div>
+                        <div className='nameproject-projects'>
+                            <h1>Name project</h1>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, rem odit? Voluptatum velit in nobis quis suscipit laborum excepturi deserunt! Vitae voluptas earum, voluptate officia illo libero porro deserunt labore.</p>
+                        </div>
+                    <div className='visualizations-projects'>
+                        <p>Visualizations</p>
+                        <FavoriteBorderIcon className='fav-icon-projects' />
+
+                    </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
-export default Projects
+
+export default Projects;
