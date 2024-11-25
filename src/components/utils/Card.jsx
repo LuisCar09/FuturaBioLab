d65b0f09bb35e8e1a2f5081a6a6f121f1cae179b
Luis Carlos <luiscbravo94@gmail.com>
2024-11-25 15:10:51 +0000

    style: added overlay to projectcard

--- src/components/utils/Card.jsx
+++ src/components/utils/Card.jsx
@@ -1,15 +1,19 @@
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Card = ({id,username,date,title,descriptionProject,projectImg,projectViews,userImg}) => {
+    const [showOverlay,setShowOverlay] = useState(false)
     return (
         <>
        
         <div className='container-projects'>
-        <Link to={`/projects/${id}`} className='link-to-project'></Link>
+        
             <div className='image-projects'>
+            <Link to={`/projects/${id}`} className={!showOverlay ? 'link-to-project ': 'link-to-project overlay'} onMouseEnter={() => setShowOverlay(!showOverlay)}  onMouseLeave={() => setShowOverlay(!showOverlay)} >
                 <img src={projectImg[0]} alt={title} />
+            </Link>
             </div>
             <div className='info-projects'>
                 <div className='usercontainer-projects'>
