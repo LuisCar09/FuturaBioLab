a6178e0f85379c9fe7574993e7f5154d3ff584c6
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-11-26 19:54:43 +0100

    styles:css to userDataregister sheet

--- src/pages/UserDataRegister.jsx
+++ src/pages/UserDataRegister.jsx
@@ -4,39 +4,40 @@ function UserDataRegister() {
     return (
         
             <section className='register-sectioncomplement'>
-                <h1 className='title-login'>Create an account</h1>
-                <form className='form-login'>
-                    <div className='name-lastname-login'> 
-                        <div className='name-input-login'>
-                            <label htmlFor='name-input' className='label-login'>Name*</label>
+                <h1 className='title-userDataRegister'>Create an account</h1>
+                <form className='form-userDataRegister'>
+                    <div className='container-input-userDataRegister'> 
+                        <div className='info-input-userDataRegister'>
+                            <label htmlFor='name-input' className='label-userDataRegister'>Name*</label>
                             <input id='name-input' type='text' required /> 
                         </div>
-                        <div className='name-input-login'>
-                            <label htmlFor='lastname-input' className='label-login'>Lastname*</label>
+                        <div className='info-input-userDataRegister'>
+                            <label htmlFor='lastname-input' className='label-userDataRegister'>Lastname*</label>
                             <input id='lastname-input' type='text' required />      
                         </div>           
                     </div> 
-                    <div className='phone-date-login'> 
-                        <div className='name-input-login'>
-                            <label htmlFor='phone-input' className='label-login'>Phone*</label>
+                    <div className='container-input-userDataRegister'> 
+                        <div className='info-input-userDataRegister'>
+                            <label htmlFor='phone-input' className='label-userDataRegister'>Phone*</label>
                             <input id='phone-input' type='text' required />   
                         </div>
-                        <div className='name-input-login'>
-                            <label htmlFor='birthDate-input' className='label-login'>Birthdate*</label>
+                        <div className='info-input-userDataRegister'>
+                            <label htmlFor='birthDate-input' className='label-userDataRegister'>Birthdate*</label>
                             <input id='birthDate-input' type='text' required placeholder='DD/MM/YYYY'  /> 
                         </div>
                     </div> 
                      
-                    <div className='preferneces-offers-login'> 
-                        <div className='name-input-login'>
-                            <label htmlFor='preferences-input' className='label-login'>Preferences (optional)</label>
+                    <div className='container-input-userDataRegister'> 
+                        <div className='info-input-userDataRegister'>
+                            <label htmlFor='preferences-input' className='label-userDataRegister'>Preferences (optional)</label>
                             <input id='preferences-input' type='text' />  
                         </div>
-                        <div className='name-input-login'>
-                            <label htmlFor='offers-input' className='label-login'>Offers (optional)</label>
+                        <div className='info-input-userDataRegister'>
+                            <label htmlFor='offers-input' className='label-userDataRegister'>Offers (optional)</label>
                             <input id='offers-input' type='text' /> 
                         </div>
                     </div>
+                <button className='button-userDataRegister'>Register</button>
                 </form>
                 <h4>The fields marked with (*) are required</h4>
             </section>
