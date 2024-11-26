0d716aa668aae4f3d95c6de54e925efacab7328e
Luis Carlos <luiscbravo94@gmail.com>
2024-11-26 16:28:12 +0000

    feat: added search engine to members

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,37 +1,39 @@
-import '../styles/Members.css';
-import { useEffect, useState } from 'react';
-
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import '../styles/Members.css';
 
 function Members() {
     const [allMembers, setAllMembers] = useState([]);
-    const [show, setShow] = useState([]);
-    const [isOpen, setIsOpen] = useState(false);
-    const [sortedMembers, setSortedMembers] = useState([]);
+    const allMemberRef = useRef()
     const [inputValue, setInputValue] = useState('')
 
     const fetchMembers = async () => {
         try {
             const response = await axios.get(import.meta.env.VITE_URL_API_FUTURA_BIOLAB + 'users');
             setAllMembers(response.data);
-            setShow(response.data); 
+            allMemberRef.current = response.data
+            console.log(allMembers);
+            
+            setShow(response.data);
         } catch (error) {
-            console.error( error);
+            console.error(error);
         }
     };
+   
+    
     
     const fetchByNameMembers = async (userName) => {
         try {
             
+            !userName ? setAllMembers(allMemberRef.current) : null
             const response = await axios.get(import.meta.env.VITE_URL_API_FUTURA_BIOLAB + 'users/username/' + userName);
-            console.log(response.data);
-            
-            
+            !response.data  ? '' : setAllMembers(response.data)
+
         } catch (error) {
-          console.error(error.message)   
+            console.error(error.message)
         }
     }
     useEffect(() => {
@@ -41,32 +43,25 @@ function Members() {
         fetchByNameMembers(inputValue)
     }, [inputValue]);
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-    
-    
-    const handleSort = () => {
-        const sorted = [...allMembers].sort((a, b) => a.name.localeCompare(b.name));
-        // setSortedMembers(sorted);
-        // setShow(sorted);
-        // setIsOpen(true);
-    };
+   
+
+
+   
 
     return (
         <main className='main-members'>
             <section className='section-members'>
                 <div className='members-title'>
                     <div className='search-container'>
-                    <input type='text' placeholder='Search user' onChange={(e) => setInputValue(e.target.value)} value={inputValue}  />
-                    
-                    <SearchIcon />
+                        <input type='text' placeholder='Search user'  onChange={(e) => setInputValue(e.target.value)} value={inputValue} />
+
+                        <SearchIcon />
                     </div>
-                    
+
                 </div>
 
-                <article  className='members-container'>
-                    {allMembers.length === 0 ? (
+                <article className='members-container'>
+                    {!allMembers || allMembers.length < 1 ? (
                         <p>...Cargando</p>
                     ) : (
                         allMembers.map((selectedMember) => (
@@ -74,11 +69,9 @@ function Members() {
                                 <div className='icon-container'>
                                     <MoreVertIcon className='moreverticon-member' />
                                 </div>
-                                {selectedMember.image ? (
-            <img src={selectedMember.image} alt={selectedMember.name} />
-        ) : (
-            <p>No image available</p> // Mensaje alternativo si no hay imagen
-        )}
+                                
+                                    <img src={selectedMember.image} alt={selectedMember.name} />
+                                
                                 <div className='member-details'>
                                     <div className='member-username'>
                                         <h3>{selectedMember.userName}</h3>
