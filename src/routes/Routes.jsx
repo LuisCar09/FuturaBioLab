7e8360ed84e08b4578b9849055d2c0050aa1fb9c
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-12-06 13:23:48 +0100

    protected routes done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -41,27 +41,28 @@ const RoutesApp = () => {
             <Header />
             <Routes>
                 <Route path='/' element={<Hero />} />
-                <Route path='/thankyou' element={<ThanKYouPage />} />
+                <Route path='/thankyou' element={<ProtectedRoute> <ThanKYouPage /></ProtectedRoute>} />
                 <Route path='/profile' element={<ProtectedRoute> <Profile /> </ProtectedRoute> } />
                 <Route path='/aboutus' element={<AboutUs />} />
                 <Route path='/contactus' element={<ContactUs />} />
                 <Route path='/setting' element={ <ProtectedRoute> <Setting />  </ProtectedRoute> } />
-                <Route path='/reservation' element={<Reservation />} />
+                <Route path='/reservation' element={<ProtectedRoute> <Reservation /> </ProtectedRoute>} />
                 <Route path='/projects' element={<Projects />} />
-                <Route path='/mycart' element={<MyCart />} />
-                <Route path='/services' element={<Services />} />
+                <Route path='/mycart' element={<ProtectedRoute><MyCart /></ProtectedRoute>} />
+                <Route path='/services' element={<ProtectedRoute><Services /></ProtectedRoute>} />
                 <Route path='/search' element={<Search />} />
                {/* <Route path ='/projectcard' element={<ProjectCard />} />*/} 
-                <Route path ='/members' element={<Members />} />
+                <Route path ='/members' element={<ProtectedRoute><Members /></ProtectedRoute>} />
                 <Route path ='/userdataregister' element={<UserDataRegister />} />
                 <Route path ='/servicedetail' element={<ServiceDetail />} />
                 <Route path='/register' element={<Register />} /> 
                 <Route path='/login' element={<Login />} /> 
-                <Route path='/services/:id' element={<ServiceCard />} /> 
+                <Route path='/services/:id' element={<ProtectedRoute><ServiceCard /></ProtectedRoute> } /> 
                 <Route path='/guide' element={<Guide />} /> 
                 <Route path='/projects/new' element={<UploadProject />} /> 
                 <Route path='/priorities' element={<Priorities />} /> 
                 <Route path='/method' element={<Method />} /> 
+                
                 
                 <Route path='/projects/:id' element={<ProjectCard />} />
                 <Route path="/members/:id" element={<Profile /> } /> 
