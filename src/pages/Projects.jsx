0d578ed8fbb6886aea1cfb8be28025685a328102
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-02-21 16:18:41 +0100

    feat: added a spinner component on projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,48 +1,48 @@
 import { useEffect,useState } from 'react';
 import Card from '../components/utils/Card';
+import Spinner from './Spinner';
 import axios from 'axios';
 import EditIcon from '@mui/icons-material/Edit';
 
 import '../styles/Projects.css'
+import '../styles/Spinner.css'
 
 function Projects() {
     const [projects,setProjects] = useState(null)
    
 
-    
+    const fetchData = async () => {
+        const dataResponse = await axios.get(import.meta.env.VITE_URL_API_FUTURA_BIOLAB + 'projects')
+        setProjects(dataResponse.data)        
+    }
     
 
     useEffect(()=>{
-        try {
-            const fetchData = async () => {
-                const dataResponse = await axios.get(import.meta.env.VITE_URL_API_FUTURA_BIOLAB + 'projectss')
-                setProjects(dataResponse.data)        
-            }
-            fetchData()
-        } catch (error) {
-            console.error(error.message)
-        }
+        
+        fetchData()
     },[])
     return (
         <main className='main-projects'>
+        {!projects ?
+            <section className='section-spinner' >
+               <Spinner />
+            </section>
+        :
             
-            {!projects ? 
-                <section className='section-spinner' >
-                    <div className='spiner-projects'></div> 
-                </section>
-            :
-            projects.map(project => {
+            <section className='section-projects'>
+                {projects.map(project => {
                     const{owner,_id,nameproject,createdAt,description,image,views} = project
-                    
                     return(
                         <Card key={_id} id={_id} username={owner} date={createdAt.split('T')[0]} title={nameproject} descriptionProject={description} projectImg={image} projectViews={views} userImg={image}
                         />
                     )
                 })}
-            
-    
-            
-        </main>
+            </section>
+        }
+       
+
+       
+    </main>
     )
 }
 
