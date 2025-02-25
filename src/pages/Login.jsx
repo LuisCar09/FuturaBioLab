ff99e25a0aa0974d48d0ae54937a2413c1cc00c7
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-02-25 10:52:51 +0100

    styles: fixed styles on login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ import { UserContext } from "../contexts/UserContext";
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import GoogleIcon from '@mui/icons-material/Google';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import XIcon from '@mui/icons-material/X';
 
 import '../styles/Login.css'
@@ -60,21 +60,8 @@ const Login = () => {
         <main className='main-login'>
              <div className="contact-container">
             <h1>Log in to your account</h1>
-            <h3>Don't have an account? <Link to="/register">Sign Up </Link> </h3>
-            <div className="providerAuth">
-                <div id="google" className="providers-logo">
-                    <GoogleIcon />
-                </div>
-                <div id="github" className="providers-logo">
-                    <GitHubIcon />
-                </div>
-                <div id="facebook" className="providers-logo">
-                    <FacebookIcon />
-                </div>
-                <div id="twitterX" className="providers-logo">
-                    <XIcon />
-                </div>
-            </div>
+            <h4>Don't have an account? <Link to="/register">Sign Up </Link> </h4>
+            
             <form  method="post" id="login-form"  className="login-form">
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email" id="email" required placeholder="email" autoComplete='true'  onChange={(event) => setUserEmail(event.target.value) } value={userEmail} />
@@ -84,11 +71,29 @@ const Login = () => {
         
                 <div className="message-login">
                 {message && <div id="message" >{message}</div>}
-                </div>
-                <button id="login" className="login" type="button" onClick={singInUser} >Login</button>
-                <button id="back" type="button"  >Back</button>
-        
+                </div>        
             </form>
+            <div className="providerAuth">
+                <div id="google" className="providers-logo">
+                    <GoogleIcon />
+                    <p>Sign up with Google</p>
+                </div>
+                <div id="github" className="providers-logo">
+                    <GitHubIcon />
+                    <p>Sign up with GitHub</p>
+                </div>
+                <div id="LinkedIn" className="providers-logo">
+                    <LinkedInIcon />
+                    <p>Sign up with LinkedIn</p>
+                </div>
+                <div id="x" className="providers-logo">
+                    <XIcon />
+                    <p>Sign up with X</p>
+                </div>
+                </div>
+            
+                <button  className="login-button" type="button" onClick={singInUser} >Login</button>
+            
            
         </div>
         </main>
