169dc8ee0fa7d8bf339515c67097c5ba12a6216c
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-03-13 18:21:29 +0100

    feat:new branch hero

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../../styles/Hero.css';
-import SectionTop from './SectionTop';
+//import SectionTop from './SectionTop';
+import First from './First.jsx';
 import GrowSection from './GrowSection';
 import ProjectsSection from './ProjectsSection.jsx';
 import NewsSection from './NewsSection.jsx'
@@ -14,7 +15,7 @@ const Hero = () => {
             
             <section className='sectionTop'>
                 
-                <SectionTop />
+                <First />
             </section>
             <section className='growSection'>
                 <GrowSection />
