2e26f7513e2945471ad9aff86dfce4d0419b6c8c
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-03-14 13:52:23 +0100

    styles: changes on animation, missed focus

--- src/components/Home/GrowSection.jsx
+++ src/components/Home/GrowSection.jsx
@@ -8,30 +8,28 @@ const GrowSection = () => {
   // useScroll observa el progreso del scroll en la sección
   const { scrollYProgress } = useScroll({
     target: ref,  // El scroll se realiza dentro de esta sección
-    offset: ["start end", "center center", "start end"], // La animación se activa cuando el 50% de la sección está en la vista
   });
 
   // Interpolamos el tamaño y la altura en función del progreso del scroll
   const size = useTransform(
     scrollYProgress, 
-    [0, 0.5, 1],  // Rango de progreso del scroll
-    ["50vw", "100vw",  "50vw"] // El círculo comienza pequeño, se expande en el centro, y luego vuelve a su tamaño original
+    [0, 1], // Progreso de scroll: desde 0 hasta 1
+    ["50vw", "100vw"] // El círculo comienza pequeño y crece hasta ocupar toda la pantalla
   );
 
   const height = useTransform(
     scrollYProgress, 
-    [0, 0.5, 1],  // Rango de progreso del scroll
-    ["50vw", "100vw",  "50vw"] // Similar al tamaño
+    [0, 1], // Progreso de scroll
+    ["50vh", "100vh"] // El círculo comienza pequeño y crece hasta ocupar toda la pantalla
   );
 
   const borderRadius = useTransform(
     scrollYProgress, 
-    [0, 0.5, 1],  // Rango de progreso del scroll
-    ["50%", "0%", "50%"] // Empieza como un círculo, se convierte en rectángulo en el centro y luego vuelve a ser círculo
+    [0, 1], // Progreso de scroll
+    ["50%", "0%"] // El círculo cambia a rectángulo y luego vuelve a ser círculo
   );
 
   return (
-    <>
     <div className="growSection" ref={ref}>
       <motion.div
         className="section-growContainer"
@@ -49,11 +47,11 @@ const GrowSection = () => {
         </button>
       </motion.div>
     </div>
-    </> 
   );
 };
 
 export default GrowSection;
+
 
 
 
